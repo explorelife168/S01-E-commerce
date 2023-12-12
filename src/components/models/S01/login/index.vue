5e851840d3b16f066fc6089baf90d34b6ee4221d
Neil Wu <Neil@fakemail.com>
2023-12-12 22:49:35 +0800

    login css更新

--- src/components/models/S01/login/index.vue
+++ src/components/models/S01/login/index.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="login-warp">
-    <div
-      class="logo"
-      :style="{ 'background-image': `url(${require('@/assets/logo.png')})` }"
-    ></div>
-    <div class="title" v-html="'Please sign in'"></div>
-    <div class="input">
-      <input type="text" placeholder="Email address" />
-      <input type="text" placeholder="Password" />
+  <div class="login-wrap">
+    <div class="login-container">
+      <div
+        class="logo"
+        :style="{ 'background-image': `url(${require('@/assets/logo.png')})` }"
+      ></div>
+      <div class="title" v-html="'Please sign in'"></div>
+      <div class="input">
+        <input type="text" placeholder="Email address" />
+        <input type="text" placeholder="Password" />
+      </div>
+      <div class="box">
+        <input type="checkbox" />
+        <div class="box-text" v-html="'Remember me'"></div>
+      </div>
+      <div class="btn-sign-in">
+        <button>Sign in</button>
+      </div>
+      <div class="data" v-html="`© 2017-${currentYears}`"></div>
     </div>
-    <div class="checkbox">
-      <input type="checkbox" v-html="'Remember me'" />
-    </div>
-    <div class="btn-sign-in">
-      <button>Sign in</button>
-    </div>
-    <div class="data" v-html="`© 2017~${currentYears}`"></div>
   </div>
 </template>
 
@@ -35,6 +38,6 @@ const getYears = () => {
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "./index.scss";
 </style>
