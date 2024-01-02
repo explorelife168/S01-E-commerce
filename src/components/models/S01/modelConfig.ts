765793841c3c896474fa59e1df6f09603178053a
Neil Wu <Neil@fakemail.com>
2024-01-02 14:49:06 +0800

    新增活動運行模組

---
+++ src/components/models/S01/modelConfig.ts
@@ -0,0 +1,11 @@
+// 運行模組
+type ModelConfig = {
+  createNewProduct: boolean;
+};
+
+const modelConfig: ModelConfig = {
+  createNewProduct: false, //新增產品畫面開關   NewProduct > index.vue
+};
+
+export default modelConfig;
+export { modelConfig };
