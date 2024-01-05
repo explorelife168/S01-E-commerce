633b5b995c63ac88bd3203c9374fd9438a004f91
Neil Wu <Neil@fakemail.com>
2024-01-05 10:10:26 +0800

    新增新增signout按鈕

--- src/components/global/Navbar/index.vue
+++ src/components/global/Navbar/index.vue
@@ -2,13 +2,29 @@
   <div class="Navbar-wrap">
     <div class="title">
       <div class="left-title" v-text="'Company name'"></div>
-      <div class="right-title"></div>
+      <div class="right-title">
+        <button class="sign-out" @click.prevent="signOut">Sign Out</button>
+      </div>
     </div>
     <div class="block"></div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import axios, { AxiosResponse } from "axios";
+import router from "@/router";
+import config from "../../../../config/dev.env";
+
+const signOut = () => {
+  const api = `${config.API_PATH}/logout`;
+  axios.post(api).then((res: AxiosResponse) => {
+    console.log(res.data);
+    if (res.data.success) {
+      router.push("/login");
+    }
+  });
+};
+</script>
 
 <style lang="scss" scoped>
 @import "./index.scss";
