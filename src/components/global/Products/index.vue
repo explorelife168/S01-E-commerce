888fa498c61ebafbb95fc1c436e2c216cf322186
Neil Wu <Neil@fakemail.com>
2024-01-08 21:19:39 +0800

    新增pinia導入更新畫面狀態

--- src/components/global/Products/index.vue
+++ src/components/global/Products/index.vue
@@ -16,7 +16,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="obj in products" :key="obj.id">
+          <tr v-for="obj in dataStore.products" :key="obj.id">
             <td>{{ obj.category }}</td>
             <td>{{ obj.title }}</td>
             <td>{{ obj.origin_price }}</td>
@@ -24,58 +24,93 @@
             <td>未啟用</td>
             <td>
               <button class="edit">編輯</button>
-              <button class="delete">刪除</button>
+              <button class="delete" @click="deleteProducts(obj.id)">
+                刪除
+              </button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-    <!-- <div class="new-product">
-      <div class="bg"></div>
-    </div> -->
   </div>
 </template>
 
 <script lang="ts" setup>
-import axios, { AxiosResponse } from "axios";
+// import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { ref } from "vue";
+import useDataStore from "../../../stores/useDataStore";
 import modelConfig from "@/components/models/S01/modelConfig";
 import config from "../../../../config/dev.env";
 
-interface Products {
-  category: string;
-  id: string;
-  image: string;
-  is_enabled: number;
-  num: boolean;
-  origin_price: number;
-  price: number;
-  title: string;
-  unit: number;
-}
-interface ProductApi {
-  products: Partial<Products>;
-}
+const dataStore = useDataStore();
 
-const products = ref(); //產品List
+// interface Products {
+//   category: string;
+//   id: string;
+//   image: string;
+//   is_enabled: number;
+//   num: boolean;
+//   origin_price: number;
+//   price: number;
+//   title: string;
+//   unit: number;
+// }
+// interface ProductApi {
+//   products: Partial<Products>;
+// }
+
+// const products = ref(); //產品List
 const modelConfigController = ref(modelConfig); //控制模型
+
 //取得產品API
-const getProducts = () => {
-  const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/products`;
+// const getProducts = () => {
+// const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/products`;
+// axios
+//   .get<ProductApi>(api)
+//   .then((response: AxiosResponse<ProductApi>) => {
+//     products.value = response.data.products;
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+// };
+
+// [API]: /api/:api_path/admin/product/:product_id
+// [說明]:  刪除產品
+//    @api_path = 'thisismycourse2'
+// [方法]: delete
+// [成功回傳]:
+//   {
+//     "success": true,
+//     "message": "已刪除產品"
+//   }
+// [失敗回傳]: 找不到產品
+//   {
+//     "success": false,
+//     "message": "找不到產品"
+//   }
+// 刪除產品
+const deleteProducts = (id: string) => {
+  const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/admin/product/${id}`;
   axios
-    .get<ProductApi>(api)
-    .then((response: AxiosResponse<ProductApi>) => {
-      products.value = response.data.products;
-      console.log(products.value);
+    .delete(api, { data: id })
+    .then((response) => {
+      if (response.data.success) console.log("刪除商品成功");
+      //產品刪除成功後再次更新products list
+      dataStore.getProducts();
     })
     .catch((error) => {
       console.error(error);
     });
 };
+
+//開啟建立新產品開關
 const createNewProduct = (): boolean => {
   return (modelConfigController.value.createNewProduct = true);
 };
-getProducts(); //執行 取得產品 API
+// getProducts(); //執行 取得產品 API
+dataStore.getProducts();
 </script>
 
 <style lang="scss" scoped>
