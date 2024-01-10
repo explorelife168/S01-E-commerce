a1d15ee4222aac39b8851166f0bd7234434f4ad3
Neil Wu <Neil@fakemail.com>
2024-01-10 15:01:35 +0800

    新增及編輯開關

--- src/components/models/S01/modelConfig.ts
+++ src/components/models/S01/modelConfig.ts
@@ -3,26 +3,12 @@ export type ModelConfig = {
   createNewProduct: boolean;
   editProducts: boolean;
   productsId: string;
-  // newProducts: NewProducts[];
 };
-
-// type NewProducts<T = number | string> = {
-//   imageUrl: string;
-//   title: string;
-//   category: string;
-//   unit: string;
-//   origin_price: T;
-//   price: T;
-//   description: string;
-//   content: string;
-//   is_enabled: string;
-// };
 
 const modelConfig: ModelConfig = {
   createNewProduct: false, //新增產品畫面開關   NewProduct > index.vue
   editProducts: true, // true -> 編輯產品 , false -> 新增產品
   productsId: "",
-  // newProducts: [],
 };
 
 export default modelConfig;
