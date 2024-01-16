aec5b68254fc1420259d46c797c1c357b58ebe21
Neil Wu <Neil@fakemail.com>
2024-01-16 13:52:25 +0800

    新增新增checkproduct開關

--- src/components/models/S01/modelConfig.ts
+++ src/components/models/S01/modelConfig.ts
@@ -1,14 +1,16 @@
 // 運行模組
 export type ModelConfig = {
   createNewProduct: boolean;
+  checkProductSwitch: boolean;
   editProducts: boolean;
   productsId: string;
 };
 
 const modelConfig: ModelConfig = {
   createNewProduct: false, //新增產品畫面開關   NewProduct > index.vue
+  checkProductSwitch: false, // see more 按鈕
   editProducts: true, // true -> 編輯產品 , false -> 新增產品
-  productsId: "",
+  productsId: "", // 確認產品Id edit用
 };
 
 export default modelConfig;
