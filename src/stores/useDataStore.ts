9c08e0bd44461f8a06f9fcd0415c4ef0f736064e
Neil Wu <Neil@fakemail.com>
2024-01-16 11:15:29 +0800

    修正產品更新資訊

--- src/stores/useDataStore.ts
+++ src/stores/useDataStore.ts
@@ -17,30 +17,45 @@ interface UpdateProductsList {
   num: number;
   image: string;
 }
+interface UpdatePagination {
+  category: null;
+  current_page: number;
+  has_next: boolean;
+  has_pre: boolean;
+  total_pages: number;
+}
 
 type UseDataStore = {
   products: UpdateProductsList[];
+  pagination: UpdatePagination;
   message: string;
   alertSwitch: boolean;
   stopMessageSwitch: boolean;
+  isLoading: boolean;
 };
 
 const useDataStore = defineStore({
   id: "Data",
   state: (): UseDataStore => ({
     products: [],
+    pagination: {} as UpdatePagination,
     message: "",
     alertSwitch: false,
     stopMessageSwitch: false,
+    isLoading: false,
   }),
   getters: {},
   actions: {
     // 產品畫面更新
-    async getProducts() {
+    async getProducts(page = 1) {
       try {
-        const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/products`;
+        this.isLoading = true;
+        const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/admin/products?page=${page}`;
         const response = await axios.get(api);
         this.products = response.data.products;
+        this.pagination = response.data.pagination;
+        console.log(response);
+        this.isLoading = false;
       } catch (error) {
         console.error(error);
       }
