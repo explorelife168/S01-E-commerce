6055cf0c4ee6da37d2c60b11aa59c00be20731f2
Neil Wu <Neil@fakemail.com>
2024-01-19 11:28:48 +0800

    客戶產品資訊頁面開關

--- src/components/models/S01/modelConfig.ts
+++ src/components/models/S01/modelConfig.ts
@@ -4,13 +4,15 @@ export type ModelConfig = {
   checkProductSwitch: boolean;
   editProducts: boolean;
   productsId: string;
+  userData: boolean;
 };
 
 const modelConfig: ModelConfig = {
-  createNewProduct: false, //新增產品畫面開關   NewProduct > index.vue
-  checkProductSwitch: false, // see more 按鈕
+  createNewProduct: false, // 新增產品畫面開關   NewProduct > index.vue
+  checkProductSwitch: false, // see more畫面開關
   editProducts: true, // true -> 編輯產品 , false -> 新增產品
   productsId: "", // 確認產品Id edit用
+  userData: false, // 客戶資訊頁面
 };
 
 export default modelConfig;
