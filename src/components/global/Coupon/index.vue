1371bf9b5cf2856f1cc94f40dbebbbaf00debb09
Neil Wu <Neil@fakemail.com>
2024-01-29 14:46:33 +0800

    新增新增coupon頁面

---
+++ src/components/global/Coupon/index.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="coupon-wrap">
+    <loading v-model:active="isLoading" />
+    <div class="create-btn">
+      <button class="btn" @click="createNewCoupon">NewCoupon</button>
+    </div>
+    <div class="coupons">
+      <table>
+        <thead>
+          <tr>
+            <td>Due_Date</td>
+            <td>Title</td>
+            <td>Code</td>
+            <td>Percent</td>
+            <td>Is_Enabled</td>
+            <td>Edit</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(coupon, index) in updateCouponList" :key="index">
+            <td>{{ dateTransfer(coupon.due_date) }}</td>
+            <td>{{ coupon.title }}</td>
+            <td>{{ coupon.code }}</td>
+            <td>{{ coupon.percent }}</td>
+            <td>{{ coupon.is_enabled == 1 ? "啟用" : "未啟用" }}</td>
+            <td>
+              <button class="edit" @click="editBtnController(coupon)">
+                Edit
+              </button>
+              <button class="delete" @click="deleteCoupon(coupon.id)">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="pagination">
+      <div class="left-btn">
+        <button
+          @click.prevent="dataStore.getCouponList(pagination.current_page - 1)"
+          class="left"
+          :class="{ disabled: !pagination.has_pre }"
+        >
+          &lt;&lt;
+        </button>
+      </div>
+
+      <div
+        class="pages-btn"
+        v-for="(pages, index) in pagination.total_pages"
+        :key="index"
+      >
+        <button
+          :class="[
+            `pages-${pages}`,
+            { active: pagination.current_page === pages },
+          ]"
+          @click.prevent="dataStore.getCouponList(pages)"
+        >
+          {{ pages }}
+        </button>
+      </div>
+      <div class="right-btn">
+        <button
+          @click.prevent="dataStore.getCouponList(pagination.current_page + 1)"
+          class="right"
+          :class="{ disabled: !pagination.has_next }"
+        >
+          >>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import axios from "axios";
+import { ref, computed } from "vue";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+import useDataStore from "../../../stores/useDataStore";
+import modelConfig from "@/components/models/S01/modelConfig";
+import config from "../../../../config/dev.env";
+// import currency from "@/utils/filters/currency"; // 小數點
+import type { Coupon } from "@/@types";
+
+// interface EditProducts {
+//   category: string;
+//   content: string;
+//   description: string;
+//   id: string;
+//   imageUrl: string;
+//   is_enabled: number;
+//   origin_price: number;
+//   price: number;
+//   title: string;
+//   unit: string;
+//   num: number;
+//   image: string;
+// }
+
+// const products = ref(); //產品List
+const dataStore = useDataStore();
+
+const modelConfigController = ref(modelConfig); //控制模型
+
+const isLoading = ref(false);
+
+// const updateCouponList = ref();
+
+// const pagination = ref({
+//   category: null,
+//   current_page: 1,
+//   has_next: true,
+//   has_pre: false,
+//   total_pages: 0,
+// });
+
+const updateCouponList = computed(() => dataStore.updateCouponList);
+
+const pagination = computed(() => dataStore.couponPagination);
+// 產品畫面顯示
+// const updateProducts = computed(() => dataStore.products);
+
+// 建立新優惠券
+const createNewCoupon = () => {
+  return (
+    (modelConfigController.value.createNewCoupon = true),
+    (modelConfigController.value.editCoupon = false)
+  );
+};
+
+const deleteCoupon = async (id: string) => {
+  isLoading.value = true;
+  const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/admin/coupon/${id}`;
+  try {
+    const response = await axios.delete(api);
+    console.log(response);
+    console.log("刪除成功");
+  } catch (error) {
+    console.log(error);
+    console.log("刪除失敗");
+  } finally {
+    dataStore.getCouponList();
+    isLoading.value = false;
+  }
+};
+//獲得CouponList
+// const getCouponList = async (page = 1) => {
+//   isLoading.value = true;
+//   const api = `${config.API_PATH}/api/${config.CUSTOM_PATH}/admin/coupons?page=${page}`;
+//   try {
+//     const response = await axios.get(api);
+//     updateCouponList.value = response.data.coupons;
+//     pagination.value = response.data.pagination;
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     isLoading.value = false;
+//   }
+// };
+
+const editBtnController = (coupon: Coupon) => {
+  modelConfigController.value.editCoupon = true;
+  modelConfigController.value.createNewCoupon = true;
+  modelConfigController.value.couponId = coupon;
+};
+
+const dateTransfer = (t: number) => {
+  if (!t) return;
+  return new Date(t * 1000).toISOString().replace("T", " ").substring(0, 19);
+};
+
+dataStore.getCouponList();
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
