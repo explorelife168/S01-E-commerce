6617a85bad39e3a8b1343a4e0b726fdf055d34bd
Neil Wu <Neil@fakemail.com>
2024-01-29 14:46:13 +0800

    新增新增coupon開關

--- src/components/models/S01/modelConfig.ts
+++ src/components/models/S01/modelConfig.ts
@@ -1,17 +1,24 @@
+import type { Coupon } from "@/@types";
 // 運行模組
 export type ModelConfig = {
   createNewProduct: boolean;
+  createNewCoupon: boolean;
   checkProductSwitch: boolean;
   editProducts: boolean;
   productsId: string;
+  editCoupon: boolean;
+  couponId: Coupon;
   userDataSwitch: boolean;
 };
 
 const modelConfig: ModelConfig = {
   createNewProduct: false, // 新增產品畫面開關   NewProduct > index.vue
+  createNewCoupon: false, // 新增優惠券畫面開關
   checkProductSwitch: false, // see more畫面開關
   editProducts: true, // true -> 編輯產品 , false -> 新增產品
   productsId: "", // 確認產品Id edit用
+  editCoupon: true,
+  couponId: {} as Coupon,
   userDataSwitch: false, // 客戶資訊頁面
 };
 
