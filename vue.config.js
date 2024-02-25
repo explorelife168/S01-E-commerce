7b158a1cd118b082e802083d23d0454241529d95
Neil Wu <Neil@fakemail.com>
2024-02-25 23:10:05 +0800

    錯誤更新

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/S01-E-commerce/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/S01-E-commerce/homepage"
+      : "/homepage",
 });
